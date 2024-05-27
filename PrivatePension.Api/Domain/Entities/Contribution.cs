0fba077e538e167ee163aff3cf90e01b9489dc48
DeividsonOmedio <deividsonomedio@outlook.com>
2024-05-27 16:54:38 -0300

    feat: criação das entidades

---
+++ PrivatePension.Api/Domain/Entities/Contribution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Contribution
+    {
+        public int Id { get; set; }
+        public int PurchaseId { get; set; }
+        public Purchase Purchase { get; set; } = new();
+        public decimal Amount { get; set; }
+        public DateTime ContributionDate { get; set; }
+    }
+}
---
+++ PrivatePension.Api/Domain/Entities/Purchase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public User Client { get; set; } = new();
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = new();
+        public DateTime PurchaseDate { get; set; }
+        public bool IsApproved { get; set; } 
+    }
+}
