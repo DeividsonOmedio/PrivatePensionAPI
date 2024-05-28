78e15d779f56eaea8ea34a17c0aea434bc53f9ed
DeividsonOmedio <deividsonomedio@outlook.com>
2024-05-28 15:31:02 -0300

    feat: add Interfaces Repositorios

---
+++ PrivatePension.Api/Domain/Interfaces/InterfacesRepositories/IPurchaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.InterfacesRepositories
+{
+    public interface IPurchaseRepository : IGenericRepository<Purchase>
+    {
+        Task<Purchase> GetByUser(User user);
+        Task<Purchase> GetByProduct(Product product);
+        Task<Purchase> GetByDate(DateTime date);
+        Task<Purchase> GetByStatus(bool status);
+    }
+}
