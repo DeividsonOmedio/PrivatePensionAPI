92ae57df0cf38b2043a9458c8926c88ffec5fe3b
DeividsonOmedio <deividsonomedio@outlook.com>
2024-05-29 00:15:47 -0300

    fix: update entities

--- PrivatePension.Api/Domain/Entities/Product.cs
+++ PrivatePension.Api/Domain/Entities/Product.cs
@@ -1,18 +1,23 @@
-﻿using Domain.Notifications;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities
 {
-    public class Product : Notifies
+    public class Product
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
         public decimal Price { get; set; }
+
+        [MaxLength(500)]
         public string Description { get; set; } = string.Empty;
+
+        [Required]
         public bool Available { get; set; }
     }
 }
